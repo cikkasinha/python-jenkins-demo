2c1ef9ec15d39c88dd9ad0ce53e24d0404820307
cikkasinha <111222480+cikkasinha@users.noreply.github.com>
2025-08-05 01:29:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins + Python + Docker!"
+    return "Hello from Jenkins + Python + Docker chandrakant ka mamla hai!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
